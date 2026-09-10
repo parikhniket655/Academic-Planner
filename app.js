8a56ecfc768ea6e8f6b861ee9442cab4cfbea9d9
Niket Parikh <ipm04niketp@iimrohtak.ac.in>
2026-09-11 01:35:10 +0530

    Bump app version to 4.0 & clear stale cache for non-cyclical Term V classes

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ try {
   })();
 
   const storedVer = parseFloat(window.localStorage.getItem("iimr_app_version") || "0");
-  if (isStorageWorking && storedVer < 3.2) {
+  if (isStorageWorking && storedVer < 4.0) {
     const activeUser = window.localStorage.getItem("iimr_active_user");
     const studentDb = window.localStorage.getItem("iimr_student_db");
     
@@ -53,7 +53,7 @@ try {
     
     if (activeUser) window.localStorage.setItem("iimr_active_user", activeUser);
     if (studentDb) window.localStorage.setItem("iimr_student_db", studentDb);
-    window.localStorage.setItem("iimr_app_version", "3.2");
+    window.localStorage.setItem("iimr_app_version", "4.0");
     
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.getRegistrations().then(registrations => {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "iimr-tracker-cache-v69";
+const CACHE_NAME = "iimr-tracker-cache-v70";
 const ASSETS = [
   "./index.html",
   "./styles.css",
